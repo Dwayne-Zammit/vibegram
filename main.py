5885dcc0bc42f90603252b6b9387780329b6d892
unknown <dwaynezammit1999@gmail.com>
2024-07-26 21:28:30 +0200

    ok

--- main.py
+++ main.py
@@ -54,4 +54,4 @@ app.register_blueprint(vibegram_related_routes)
 socketio_bp(socketio)
 
 if __name__ == '__main__':
-    wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
+    app.run(host='0.0.0.0', port=5000)
